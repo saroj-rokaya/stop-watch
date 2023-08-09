f063cadec0573c731dbcab3c2b093b55c5399059
Saroj Kumar Rokaya <110958773+saroj-rokaya@users.noreply.github.com>
2023-08-09 21:19:46 +0545

    stop watch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-container">
+      <div className="timer-container">
+        <h1>00:00:00</h1>
+        <button>START</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
