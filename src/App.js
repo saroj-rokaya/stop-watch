ca5e9b1484a5a8433fd7b6edd3c43cf1b22b285e
Saroj Kumar Rokaya <110958773+saroj-rokaya@users.noreply.github.com>
2023-08-10 23:43:48 +0545

    logic building

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [time,setTime]=useState({ms:0,sec:0,min:0,hrs:0});
+  function update(){
+    
+  }
   return (
     <div className="main-container">
       <div className="timer-container">
-        <h1>00:00:00</h1>
-        <button>START</button>
+        <span>{(time.ms>=10)?(time.ms):"0"+(time.ms)}</span>
+        <span>{(time.sec>=10)?(time.sec):"0"+(time.sec)}</span>
+        <span>{(time.min>=10)?(time.min):"0"+(time.min)}</span>
+        <span>{(time.hrs>=10)?(time.hrs):"0"+(time.hrs)}</span>
+        
+        <button onClick={update}>START</button>s
       </div>
     </div>
   );
